54633dde2053c2b86ed53f133b9a935070de852f
josego <josemagomez53@gmail.com>
2025-01-13 17:57:56 +0100

    updated gradle, added libraries, added navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     id("com.google.gms.google-services")
+    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
@@ -44,16 +45,26 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.legacy.support.v4)
+    implementation(libs.androidx.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    
+
+    //Navigation Component
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+    //LiveData
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+
     //FireBase
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
     implementation(libs.firebase.auth.ktx)
     implementation(libs.google.firebase.auth)
     implementation(libs.firebase.firestore)
+    implementation("com.google.firebase:firebase-auth-ktx:22.3.0")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    id ("com.android.library") version "8.7.2" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.8.4" apply false
 }
